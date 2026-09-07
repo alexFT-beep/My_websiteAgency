365bc299f7fb5c808451b9258594913a07e21dfd
alex FT <alberto900957415@gmail.com>
2026-09-07 13:18:25 -0500

    fix: restore complete faq.astro without self-import, restore clean package.json and astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://albertotejada-mywebsite.com',
-  adapter: vercel()
 });
